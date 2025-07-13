5babdce06f0d79e6e66cbd775e9d6875207c3f80
Henrique Mendes <henrique.mendes@mindsource.pt>
2025-07-14 00:47:43 +0100

    Update ContractList.vue
    Change of tailwind style :)

--- src/views/ContractList.vue
+++ src/views/ContractList.vue
@@ -18,37 +18,39 @@
     </div>
 
 
-    <table class="min-w-full table-fixed text-sm border-collapse shadow-xl rounded overflow-hidden"
-      v-loading="isLoading">
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 
-      <thead class="text-left text-sm font-semibold text-gray-900 dark:text-white ">
-        <tr>
-          <th class="p-3">#</th>
-          <th class="p-3">{{ $t('legalEntityName') }}</th>
-          <th class="p-3">{{ $t('author') }}</th>
-          <th class="p-3">{{ $t('description') }}</th>
-          <th class="p-3">{{ $t('createdAt') }}</th>
-          <th class="p-3 text-center">{{ $t('actions') }}</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(contract, index) in contracts" :key="contract.id" class="border-b hover:bg-gray-50">
-          <td class="p-3 text-sm text-gray-900 dark:text-white">{{ index + 1 }}</td>
-          <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.legalEntityName }}</td>
-          <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.authorName }}</td>
-          <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.description }}</td>
-          <td class="p-3 text-sm text-gray-500">{{ formatDate(contract.createdAt) }}</td>
+      <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" v-loading="isLoading">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th class="p-3">#</th>
+            <th class="p-3">{{ $t('legalEntityName') }}</th>
+            <th class="p-3">{{ $t('author') }}</th>
+            <th class="p-3">{{ $t('description') }}</th>
+            <th class="p-3">{{ $t('createdAt') }}</th>
+            <th class="p-3 text-center">{{ $t('actions') }}</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
+            v-for="(contract, index) in contracts" :key="contract.id">
+            <td class="p-3 text-sm text-gray-900 dark:text-white">{{ index + 1 }}</td>
+            <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.legalEntityName }}</td>
+            <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.authorName }}</td>
+            <td class="p-3 text-sm text-gray-900 dark:text-white">{{ contract.description }}</td>
+            <td class="p-3 text-sm text-gray-500">{{ formatDate(contract.createdAt) }}</td>
 
-          <td class="p-3 text-sm text-center">
-            <div class="flex flex-col sm:flex-row justify-center items-center gap-2">
-              <el-button type="primary" @click="editContract(contract)">{{ $t('edit') }}</el-button>
-              <el-button type="danger" @click="deleteContract(contract)">{{ $t('delete') }}</el-button>
-            </div>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-
+            <td class="p-3 text-sm text-center">
+              <div class="flex flex-col sm:flex-row justify-center items-center gap-2">
+                <el-button type="primary" @click="editContract(contract)">{{ $t('edit') }}</el-button>
+                <el-button type="danger" @click="deleteContract(contract)">{{ $t('delete') }}</el-button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <div class="mt-6 flex justify-center">
       <el-pagination layout="total, prev, pager, next" v-model:current-page="currentPage" v-model:page-size="pageSize"
         :total="totalContracts" background />
@@ -66,7 +68,10 @@ import ContractModal from '@/components/ContractModal.vue';
 import type { Contract } from '../types/Contract';
 import { ElMessageBox, ElMessage } from 'element-plus';
 import formatDateUtil from '@/utils/formatDate';
+import { useI18n } from 'vue-i18n';
 
+
+const { t } = useI18n();
 
 const contractStore = useContractStore();
 const modalRef = ref<InstanceType<typeof ContractModal> | null>(null);
@@ -99,18 +104,18 @@ const editContract = (contract: Contract) => {
 }
 const deleteContract = (contract: Contract) => {
   ElMessageBox.confirm(
-    `Are you sure you want to delete "${contract.legalEntityName}"?`,
-    'Confirm Delete',
+    t('delete-confirmation', { legalEntityName: contract.legalEntityName }),
+    t('confirm-delete'),
     {
-      confirmButtonText: 'Delete',
-      cancelButtonText: 'Cancel',
+      confirmButtonText: t('delete'),
+      cancelButtonText: t('cancel'),
       type: 'warning',
     }
   )
     .then(async () => {
       await contractStore.deleteContract(contract.id);
       await contractStore.loadContractsWithPagination(1, pageSize);
-      ElMessage.success('Contract deleted successfully');
+      ElMessage.success(t('contract-deleted-successfully'));
     })
     .catch(() => {
       /* cancelado */
